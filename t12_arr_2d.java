4a974b8a07dfb5de63d42e290183b7840d16065a
Hyowon Choi / 최효원 <gydnjs1231@yonsei.ac.kr>
2023-01-17 14:27:32 +0900

    Add files via upload

---
+++ t12_arr_2d.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int N = 50;
+		int[][] arr = new int[N][N];
+		for(int i = 0;i < N; i ++) {
+			for(int j  = 0; j < N; j++) {
+				
+				arr[i][j] = (int)(Math.random()*10); 
+				
+			}
+		}
+		for(int i = 0;i < N; i ++) {
+			
+			for(int j  = 0; j < N; j++) {
+				
+				System.out.print(arr[i][j]+ " ");	
+			}
+			System.out.println(); 
+		}
+
+	}
+
+}
